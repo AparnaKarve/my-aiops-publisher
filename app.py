df7133ff295b3536e9e4653c8b8022bf23a6b2c0
Aparna Karve <aparna.karve@gmail.com>
2018-10-25 15:56:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ def wake_up():
     topic = os.environ.get('KAFKA_TOPIC')
     # available_message = os.environ.get('KAFKA_AVAILABLE_MESSAGE') -- ?
 
-    aws_key = os.environ.get('AWS_ACCESS_KEY_ID')
-    aws_secret = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    aws_bucket = os.environ.get('AWS_S3_BUCKET_NAME')
+    # aws_key = os.environ.get('AWS_ACCESS_KEY_ID')
+    # aws_secret = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    # aws_bucket = os.environ.get('AWS_S3_BUCKET_NAME')
 
-    filesystem = s3.connect(aws_key, aws_secret)
-    s3.save_data(filesystem, aws_bucket, "Hello AIOPS")
+    # filesystem = s3.connect(aws_key, aws_secret)
+    # s3.save_data(filesystem, aws_bucket, "Hello AIOPS")
     producer.publish_message(server, topic, 'available')
     return 'Hello World!'
 
